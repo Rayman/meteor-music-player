9cf685d7f20d5f5117ad6ec6639dd75c71bb65dc
Ramon Wijnands <rayman747@hotmail.com>
2014-10-24 21:32:34 +0200

    upgrade meteor to 0.9.4

--- packages/music-player/package.js
+++ packages/music-player/package.js
@@ -1,20 +1,23 @@
 Package.describe({
-  summary: "A reactive music player with various backends"
+  name: 'music-player',
+  summary: 'A reactive music player with various backends',
+  version: '0.2.0',
+  git: 'https://github.com/Rayman/meteor-music-player.git'
 });
 
-Package.on_use(function (api, where) {
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.4');
+
   api.use("coffeescript", "client");
-  api.add_files('music-player.coffee', 'client');
-  api.add_files('backend.coffee', 'client');
 
-  api.export && api.export(['MusicPlayer'], 'client');
-
+  api.addFiles('music-player.coffee');
+  api.addFiles('backend.coffee');
+  
+  api.export('MusicPlayer', ['client']);
 });
 
-/*
-Package.on_test(function (api) {
-  api.use("coffeescript", "client");
+Package.onTest(function(api) {
+  api.use('tinytest');
   api.use('music-player');
-  api.add_files('music-player_tests.coffee', 'client');
+  api.addFiles('music-player-tests.js');
 });
-*/
